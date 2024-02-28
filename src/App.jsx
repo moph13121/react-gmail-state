81926f430ace342e88404997948bd1b0bd2ff84f
Philip Morud <philip.morud@no.experis.com>
2024-02-28 09:03:06 +0100

    Core and Extensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,107 @@
 import Header from './components/Header'
 import initialEmails from './data/emails'
+import { useState } from 'react'
 
 import './styles/App.css'
 
+const getReadEmails = emails => emails.filter(email => !email.read)
+
+const getStarredEmails = emails => emails.filter(email => email.starred)
+
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails)
+  const [hideRead, setHideRead] = useState(false)
+  const [currentTab, setCurrentTab] = useState('inbox')
+
+  const unreadEmails = emails.filter(email => !email.read)
+  const starredEmails = emails.filter(email => email.starred)
+
+  const toggleStar = targetEmail => {
+    const updatedEmails = emails =>
+      emails.map(email =>
+        email.id === targetEmail.id
+          ? { ...email, starred: !email.starred }
+          : email
+      )
+    setEmails(updatedEmails)
+  }
+
+  const toggleRead = targetEmail => {
+    const updatedEmails = emails =>
+      emails.map(email =>
+        email.id === targetEmail.id ? { ...email, read: !email.read } : email
+      )
+    setEmails(updatedEmails)
+  }
+
+  let filteredEmails = emails
+
+  if (hideRead) filteredEmails = getReadEmails(filteredEmails)
+
+  if (currentTab === 'starred')
+    filteredEmails = getStarredEmails(filteredEmails)
 
   return (
     <div className="app">
       <Header />
       <nav className="left-menu">
-        <ul className="inbox-list">
-          <li
-            className="item active"
-            // onClick={() => {}}
-          >
-            <span className="label">Inbox</span>
-            <span className="count">?</span>
-          </li>
-          <li
-            className="item"
-            // onClick={() => {}}
-          >
-            <span className="label">Starred</span>
-            <span className="count">?</span>
-          </li>
+            <ul className="inbox-list">
+                <li
+                    className={`item ${currentTab === 'inbox' ? 'active' : ''}`}
+                    onClick={() => setCurrentTab('inbox')}
+                >
+                    <span className="label">Inbox</span>
+                    <span className="count">{unreadEmails.length}</span>
+                </li>
+                <li
+                    className={`item ${currentTab === 'starred' ? 'active' : ''}`}
+                    onClick={() => setCurrentTab('starred')}
+                >
+                    <span className="label">Starred</span>
+                    <span className="count">{starredEmails.length}</span>
+                </li>
 
-          <li className="item toggle">
-            <label for="hide-read">Hide read</label>
-            <input
-              id="hide-read"
-              type="checkbox"
-              checked={false}
-              // onChange={() => {}}
-            />
-          </li>
+                <li className="item toggle">
+                    <label htmlFor="hide-read">Hide read</label>
+                    <input
+                        id="hide-read"
+                        type="checkbox"
+                        checked={hideRead}
+                        onChange={e => setHideRead(e.target.checked)}
+                    />
+                </li>
+            </ul>
+        </nav>
+      <main className="emails">
+        <ul>
+          {filteredEmails.map((email, index) => (
+            <li
+              key={index}
+              className={`email ${email.read ? 'read' : 'unread'}`}
+            >
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                  checked={email.read}
+                  onChange={() => toggleRead(email)}
+                />
+              </div>
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  checked={email.starred}
+                  onChange={() => toggleStar(email)}
+                />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
         </ul>
-      </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      </main>
     </div>
   )
 }
